1b0690ea13bc7923f855ece7c14d512273f83176
gauravgs-dot <gaurav.salian2005@gmail.com>
2026-08-09 22:14:55 +0530

    Add encryption_program

---
+++ encryption_program.py
@@ -0,0 +1,22 @@
+import random
+import string
+chars=" "+string.punctuation+string.digits+string.ascii_letters
+chars=list(chars)
+key=chars.copy()
+random.shuffle(key)
+#ENCRYPT
+plain_text=input("Enter the text to encrypt: ")
+cipher_text=" "
+for letter in plain_text:
+    index=chars.index(letter)
+    cipher_text+=key[index]
+print(f"Plain text: {plain_text}")
+print(f"Encrypted text: {cipher_text}")
+#DECRYPT
+cipher_text=input("Enter a message to Decrypt:")
+plain_text=" "
+for letter in cipher_text:
+    index=key.index(letter)
+    plain_text+=chars[index]
+print(f"Encrypted text: {cipher_text}")   
+print(f"Original text: {plain_text}")
